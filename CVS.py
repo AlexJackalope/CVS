5d83bde1d7402e2ccc1fb6b4d7ca130186309ce5
Alexandra Zaytseva <62497070+AlexJackalope@users.noreply.github.com>
2021-05-03 10:06:27 +0500

    OS independent file creating and checking repository before 'add'

--- CVS.py
+++ CVS.py
@@ -8,15 +8,34 @@ def is_dir_empty(path):
     return not os.listdir(path)
 
 
+def check_repository(path):
+    repository = os.path.join(path, "repository")
+    if not does_dir_exist(repository):
+        sys.exit("Repository is not initialized, call 'init' in an empty folder to do it.")
+    objects = does_dir_exist(os.path.join(repository, "objects"))
+    index = os.path.exists(os.path.join(repository, "index.bin")) and \
+            os.path.isfile(os.path.join(repository, "index.bin"))
+    logs = os.path.exists(os.path.join(repository, "logs.bin")) and \
+           os.path.isfile(os.path.join(repository, "logs.bin"))
+    if not (objects and index and logs):
+        sys.exit("Repository is damaged.")
+
+
 def init(path):
     if not is_dir_empty(path):
         sys.exit("To initialize a repository choose an empty folder")
-    repository = path + '\\repository'
-    os.mkdir(repository)
-    os.mkdir(repository + '\\objects')
-    Path(repository + "\\index.bin").touch()
-    Path(repository + "\\logs.bin").touch()
+    os.mkdir(os.path.join(path, "repository"))
+    os.mkdir(os.path.join(path, "repository", "objects"))
+    Path(os.path.join(path, "repository", "index.bin")).touch()
+    Path(os.path.join(path, "repository", "logs.bin")).touch()
     print("Repository initialized")
+
+
+def add(path, files):
+    check_repository(path)
+    print("Repository is OK")
+    for directory in path:
+        print("a")
 
 
 def does_dir_exist(path):
@@ -40,6 +59,8 @@ def main():
         sys.exit("Given directory does not exist")
     if args.command == "init":
         init(args.path)
+    if args.command == "add":
+        add(args.path, args.files)
 
 
 if __name__ == '__main__':
